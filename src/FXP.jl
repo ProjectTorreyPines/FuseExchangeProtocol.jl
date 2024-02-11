f622e651d0432dde2dd3b7edade0dfb37ad184ef
Orso Meneghini <orso82@gmail.com>
2024-02-10 22:29:36 -0800

    refactoring

--- src/FXP.jl
+++ src/FXP.jl
@@ -6,22 +6,31 @@ import Jedis: Client, disconnect!
 
 const sep = "__"
 
+function new_client_copy(client_in::Jedis.Client)
+    return Jedis.Client(;
+        client_in.host,
+        client_in.port,
+        client_in.database,
+        client_in.password,
+        client_in.username)
+end
+
 function Base.push!(client::Jedis.Client, session_id::String, service_name::String, whoami::Symbol; data...)
-    @assert whoami ∈ [:service, :client]
-    if whoami == :service
-        key = join((session_id, service_name, "srv2cli"), sep)
-    elseif whoami == :client
-        key = join((session_id, service_name, "cli2srv"), sep)
+    @assert whoami ∈ [:provider, :requestor]
+    if whoami == :provider
+        key = join((session_id, service_name, "pro2req"), sep)
+    elseif whoami == :requestor
+        key = join((session_id, service_name, "req2pro"), sep)
     end
     return Jedis.lpush(key, JSON.sprint(data); client)
 end
 
 function Base.pop!(client::Jedis.Client, session_id::String, service_name::String, whoami::Symbol; timeout::Float64, error_on_timeout::Bool=true)
-    @assert whoami ∈ [:service, :client]
-    if whoami == :service
-        key = join((session_id, service_name, "cli2srv"), sep)
-    elseif whoami == :client
-        key = join((session_id, service_name, "srv2cli"), sep)
+    @assert whoami ∈ [:provider, :requestor]
+    if whoami == :provider
+        key = join((session_id, service_name, "req2pro"), sep)
+    elseif whoami == :requestor
+        key = join((session_id, service_name, "pro2req"), sep)
     end
     data = Jedis.brpop(key; timeout, client)
     if isempty(data)
@@ -34,11 +43,18 @@ function Base.pop!(client::Jedis.Client, session_id::String, service_name::Strin
     return Dict(Symbol(k) => v for (k, v) in JSON.parse(data[2]))
 end
 
-function register_service(client::Jedis.Client, service_name::String, service_function::Function)
+"""
+    register_service(client::Jedis.Client, service_name::String, service_function::Function; timeout::Float64=10.0)
+
+`service_function` must have the following call signature:
+
+    service_function(client::Jedis.Client, session_id::String, service_name::String; timeout::Float64=10.0)
+"""
+function register_service(client::Jedis.Client, service_name::String, service_function::Function; timeout::Float64=10.0)
     @async Jedis.subscribe(service_name; client) do msg
         session_id = msg[3]
         service_name = msg[2]
-        return service_function(session_id, service_name)
+        return service_function(client, session_id, service_name; timeout)
     end
     return Jedis.wait_until_subscribed(client)
 end
@@ -50,11 +66,22 @@ function has_service_provider(client::Jedis.Client, service_name::String)
 end
 
 function negotiate_service(client::Jedis.Client, session_id::String, service_name::String)
-    key = join((session_id, service_name, "srv2cli"), sep)
+    key = join((session_id, service_name, "pro2req"), sep)
     Jedis.del(key; client)
-    key = join((session_id, service_name, "cli2srv"), sep)
+    key = join((session_id, service_name, "req2pro"), sep)
     Jedis.del(key; client)
     return Jedis.publish(service_name, session_id; client)
+end
+
+"""
+    JSON.sprint(data, args...; kw...)
+
+Return JSON represntation of data
+"""
+function JSON.sprint(data, args...; kw...)
+    buf = IOBuffer()
+    JSON.print(buf, data, args...; kw...)
+    return String(take!(buf))
 end
 
 end # module FXP
