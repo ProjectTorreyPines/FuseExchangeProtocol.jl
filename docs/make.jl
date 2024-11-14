5adcf1a4f6f7068040fec6c1ed67e5b228866ba9
Orso Meneghini <orso82@gmail.com>
2024-11-14 14:36:52 -0800

    add analytics

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ end
 
 makedocs(;
     modules=[FuseExchangeProtocol],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(;analytics="G-65D8V8C8VQ"),
     sitename="FuseExchangeProtocol",
     checkdocs=:none,
     pages=["index.md", "api.md", "License" => "license.md", "Notice" => "notice.md"]
